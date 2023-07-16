fd01ade0209e4c62489e0cdd756b420fabe857dd
Tomas Paez <paeztms@gmail.com>
2023-07-16 14:37:32 -0300

    add client app as allowed origin

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3500;
 connectDB();
 
 //MIDDLEWARE
-app.use(cors());
+app.use(cors({ origin: 'https://blog-client-ten.vercel.app' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(logger);
